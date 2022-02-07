ea399ccf0144e75d981920c312337295cce60f45
Ruben <atosc08@gmail.com>
2022-02-07 13:13:54 +0100

    Try to fix Linter Github Action part 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 export { LoginUserInput, RegisterUserInput, Token } from './Auth';
+export { CreateFeedbackInput, ValidateFeedbackParams, DeleteFeedbackParams } from './Feedbacks';
 export {
   CreateLevelInput, FindLevelParams, Levels, Types,
 } from './Levels';
 export { Users, Roles } from './Users';
 export { CreateWorldInput, Worlds } from './Worlds';
-export { CreateFeedbackInput, ValidateFeedbackParams, DeleteFeedbackParams } from './FeedBacks';
