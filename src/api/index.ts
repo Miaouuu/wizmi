0baa360838d97fbf146875c3f37f151a2fc4d9e7
Ruben <atosc08@gmail.com>
2022-02-08 23:10:35 +0100

    Releases in index

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,11 @@
 export { LoginUserInput, RegisterUserInput, Token } from './Auth';
+export { CreateChangelogInput, Changelogs } from './Changelogs';
 export {
   CreateFeedbackInput, ValidateFeedbackParams, DeleteFeedbackParams, Feedbacks,
 } from './Feedbacks';
 export {
   CreateLevelInput, FindLevelParams, Levels, Types,
 } from './Levels';
+export { CreateReleaseInput, FindReleaseParams, Releases } from './Releases';
 export { Users, Roles } from './Users';
 export { CreateWorldInput, Worlds } from './Worlds';
